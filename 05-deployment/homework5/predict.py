a59ee3aa412c97e8dd2cd0bb6ab420c0e7b36f06
caro6852lq <liccvergara@gmail.com>
2025-10-27 22:42:11 +0000

    Homework5

---
+++ 05-deployment/homework5/predict.py
@@ -0,0 +1,31 @@
+
+import pickle
+
+from typing import Dict, Any
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(tittle="converted-prediction")
+
+with open ('pipeline_v1.bin', 'rb') as f_in:
+       pipeline = pickle.load(f_in)
+
+def predict_single(customer):
+    result = pipeline.predict_proba(customer)[0,1]
+    return float(result)
+
+
+@app.post("/predict")
+def predict(customer: Dict[str, Any]):
+    prob = predict_single(customer)
+
+    result = {
+        'converted_probability': prob,
+        'converted': bool(prob >= 0.5)
+    }
+
+    return result
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=9696)
